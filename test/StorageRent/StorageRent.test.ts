f95f74bdbc2475331367186253ab153657c091ea
Pedro Moraes <pmoraes@esapiens.com.br>
2025-02-06 07:49:56 -0300

    feat: add test to verify due date in february

--- test/StorageRent/StorageRent.test.ts
+++ test/StorageRent/StorageRent.test.ts
@@ -73,4 +73,48 @@ describe('calculateMonthlyRent function', () => {
 
     expect(result).toEqual(expectedResult)
   })
+
+  it('should return MonthlyRentRecords validate if put correct the last day in due rent ', () => {
+    const contract = {
+      baseMonthlyRent: 100.0,
+      leaseStartDate: new Date('2023-01-02T00:00:00'),
+      windowStartDate: new Date('2023-01-02T00:00:00'),
+      windowEndDate: new Date('2023-04-31T00:00:00'),
+      dayOfMonthRentDue: 31,
+      rentRateChangeFrequency: 1,
+      rentChangeRate: 0.1,
+    }
+
+    const result = calculateMonthlyRent(contract)
+
+    const expectedResult = [
+      {
+        vacancy: false,
+        rentAmount: 96.67,
+        rentDueDate: new Date('2023-01-02T00:00:00'),
+      },
+      {
+        vacancy: false,
+        rentAmount: 100.0,
+        rentDueDate: new Date('2023-01-31T00:00:00'),
+      },
+      {
+        vacancy: false,
+        rentAmount: 110.0,
+        rentDueDate: new Date('2023-02-28T00:00:00'),
+      },
+      {
+        vacancy: false,
+        rentAmount: 121.0,
+        rentDueDate: new Date('2023-03-31T00:00:00'),
+      },
+      {
+        vacancy: false,
+        rentAmount: 133.1,
+        rentDueDate: new Date('2023-04-30T00:00:00'),
+      },
+    ]
+
+    expect(result).toEqual(expectedResult)
+  })
 })
